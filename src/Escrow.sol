edf5edbcfef589a44e962504da34f2dfdc4a56fc
Nadia Mahyaee <nadiamahyaee@gmail.com>
2025-07-11 18:15:01 +0330

    add main function to contract

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract Escrow {
+
+    error InvalidDeadline(uint256 time, string message);
+    error InvalidAmount(uint256 amount, string message);
+    error InvalidPayeeAddress(address payee, string message);
+
+    event NewAgreement(uint indexed agreementId, address payerAddress, uint256 amount);
+
+    uint public nextAgreementId = 0;
+    mapping(uint => Agreement) public agreements;
+
     enum State {
-        Created,
         Funded,
         Completed,
-        Cnaceled,
+        Canceled,
         InDispute
-    }
+    };
 
-    struct Aggrement {
+    struct Agreement {
         address payable payer;
         address payable payee;
         address arbiter;
@@ -19,5 +28,25 @@ contract Escrow {
         State currentState;
         bool payerConfirmed;
         bool payeeConfirmed;
+    };
+
+    function createAgreement(address _payee, address _arbiter, uint256 _deadline) public payable{
+
+        if (_deadline < block.timestamp){
+            revert InvalidDeadline(_deadline, "Deadline should be time in future");
+        }
+        if(msg.value <= 0){
+            revert InvalidAmount(msg.value, "The entered amount value in too low");
+        }
+        if(_payee == address(0)){
+            revert InvalidPayeeAddress(_payee, "The payee address in Invalid");
+        }
+
+        uint agreementId = nextAgreementId + 1;
+        Agreement memory newAgreement = Agreement(msg.sender, _payee, _arbiter, msg.value, _deadline, State.Funded, false, false);
+        
+        agreements[agreementId] = newAgreement;
+        nextAggrementId++;
+        emit NewAgreement(agreementId, msg.sender, msg.value);
     }
 }
